b75e9106f8278f9e8f63803f32a6138a578ea1c3
JTSnow19 <jacobtmartin7@gmail.com>
2024-06-01 19:22:02 -0700

    w06 binary search trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -8,7 +8,10 @@ public class Node {
     }
 
     public void Insert(int value) {
-        if (value < Data) {
+        if (value ==Data){
+            //this is the funniest solution
+        }
+        else if (value < Data) {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
@@ -26,11 +29,35 @@ public class Node {
 
     public bool Contains(int value) {
         // TODO Start Problem 2
-        return false;
+        if (value==Data){
+            return true;
+        }else if (Right is null && Left is null){
+            return false;
+        }    
+        else if(value<Data ){
+            if (Left is not null){
+                return Left.Contains(value);
+            } else{return false;}
+        }else if (value> Data){
+            if (Right is not null){
+                return Right.Contains(value);
+            } else{return false;}
+        }else{
+            return false;
+        }
     }
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (this is null){
+            return 0;
+        }
+        int leftCount =(Left is not null)? Left.GetHeight(): 0;
+        int rightCount =(Right is not null)? Right.GetHeight(): 0; //gameplan. loop through, for each branch. when a branch ends. return value
+        if (leftCount > rightCount)
+            return leftCount+1;
+        else{
+            return rightCount+1;
+        }
     }
 }
